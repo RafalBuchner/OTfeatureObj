c3bd20131bce6b1663cbeb78770cdfe2568ad70a
renual27 <rafal.buchner@gmail.com>
2018-11-08 20:24:05 +0100

    classes

--- tests/tools.py
+++ tests/tools.py
@@ -2,304 +2,312 @@ import os
 import re
 import copy
 
-special_Characters = ( "[", "]", "{", "}", ";", "#" )
-special_Expressions = ( "languagesystem", "feature", "lookup", "pos", "sub", "by" )
+special_Characters = ("[", "]", "{", "}", ";", "#")
+special_Expressions = ("languagesystem", "feature",
+                       "lookup", "pos", "sub", "by")
 
-def replaceAtIndex( l1, l2, i):
-	l2.reverse()
-	del l1[i]
-	for x in l2:
-		l1.insert(i,x)
-	return l1
+
+def replaceAtIndex(l1, l2, i):
+    l2.reverse()
+    del l1[i]
+    for x in l2:
+        l1.insert(i, x)
+    return l1
+
 
 def separate_txt_by(txt, sep):
-	import re
-	return [x for x in re.split(f"({sep})", txt) if x != '']
+    import re
+    return [x for x in re.split(f"({sep})", txt) if x != '']
+
 
 def getItemsBetweenElemnts(array, openingEl, closingEl):
-	openAdding = False
-	list_of_values = []
-	value = []
-	for x in array:
-		if x == openingEl: 
-			value = []
-			openAdding = True
-		elif x == closingEl and openAdding == True:
-			list_of_values.append(value)
-			openAdding = False
-		else:
-			if openAdding:
-				value.append(x)
+    openAdding = False
+    list_of_values = []
+    value = []
+    for x in array:
+        if x == openingEl:
+            value = []
+            openAdding = True
+        elif x == closingEl and openAdding == True:
+            list_of_values.append(value)
+            openAdding = False
+        else:
+            if openAdding:
+                value.append(x)
 
-	return list_of_values
+    return list_of_values
+
 
 def stripFea(path):
 
-	assert path.split('/')[-1].split('.')[-1] == "fea", "passed file is not a OT-feature file"
-	file = open(path,"r") 
-	fea_text =  file.read()
-	fea_text_list_temp = []
-	for element in fea_text.split(' '):
-		if len(element) > 0:
-			for sub_element in separate_txt_by(element,'\n'):
+    assert path.split(
+        '/')[-1].split('.')[-1] == "fea", "passed file is not a OT-feature file"
+    file = open(path, "r")
+    fea_text = file.read()
+    fea_text_list_temp = []
+    for element in fea_text.split(' '):
+        if len(element) > 0:
+            for sub_element in separate_txt_by(element, '\n'):
 
-				fea_text_list_temp.append(sub_element)
+                fea_text_list_temp.append(sub_element)
 
-	for i,element in enumerate(fea_text_list_temp):
-		if "\t" in element:
-			fea_text_list_temp[i] = element.replace("\t","")
+    for i, element in enumerate(fea_text_list_temp):
+        if "\t" in element:
+            fea_text_list_temp[i] = element.replace("\t", "")
 
+    fea_text_list = []
+    for element in fea_text_list_temp:
+        skip = False  # flow control
+        temp = []
+        for char in special_Characters:
+            if char in element:
+                position = element.find(char)
+                new_element = element.replace(char, "")
+                more_special_chars = False
 
+                for char_b in special_Characters:
+                    if char_b in new_element:
+                        more_special_chars = True
 
-	fea_text_list = []
-	for element in fea_text_list_temp:
-		skip = False # flow control
-		temp = []
-		for char in special_Characters:
-			if char in element:
-				position = element.find(char)
-				new_element = element.replace(char, "")
-				more_special_chars = False
+                if more_special_chars:
+                    temp.append(char)
+                    continue
 
-				for char_b in special_Characters:
-					if char_b in new_element:
-						more_special_chars = True
+                if position == 0 and new_element != "":
+                    fea_text_list.append(char)
+                    fea_text_list.append(new_element)
+                    skip = True
 
-				if more_special_chars:
-					temp.append(char)
-					continue
+                elif element[position] == element[-1] and new_element != "":
+                    fea_text_list.append(new_element)
+                    fea_text_list.append(char)
 
-				if position == 0 and new_element != "":
-					fea_text_list.append(char)
-					fea_text_list.append(new_element)
-					skip = True
+                    skip = True
+        if skip:
+            skip = False
+            continue
 
-				elif element[position] == element[-1] and new_element != "":
-					fea_text_list.append(new_element)
-					fea_text_list.append(char)
+        if element != "":
+            for char in special_Characters:
+                if char in element and len(element) > 1:
+                    element = element.replace(char, "")
 
-					skip = True
-		if skip:
-			skip = False
-			continue
+            fea_text_list.append(element)
 
-		if element != "":
-			for char in special_Characters:
-				if char in element and len(element) > 1:
-					element = element.replace(char, "")
+            if len(temp) > 0:
+                for char in temp:
+                    fea_text_list.append(char)
 
-			fea_text_list.append(element)
+    for x in range(3):  # getting rid of unnescessary line breaks
+        for i, element in enumerate(fea_text_list):
+            if i + 1 < len(fea_text_list):
+                if element == "\n" and fea_text_list[i + 1] == element:
+                    del fea_text_list[i + 1]
 
-
-			if len(temp) > 0:
-				for char in temp:
-					fea_text_list.append(char)
-
-
-	for x in range(3): # getting rid of unnescessary line breaks
-		for i,element in enumerate(fea_text_list):
-			if i+1 < len(fea_text_list):
-				if element == "\n" and fea_text_list[i+1] == element:
-					del fea_text_list[i+1]
-
-	return fea_text_list
-
-def findNestedPairs(feaList,openingEl,closingEl):
-	# returns dict, key:index in the fea list, value: (pairNumber, is opening element, nesting level)
-	countOpening = -1
-	countClosing = -1
-	countPairs = -1
-	pairDict = {} 
-
-	ascendingList = []
-	nestingList = []
-	for i, el in enumerate(feaList):
-
-		if el == openingEl:
-			countOpening += 1
-			countPairs += 1
-			nestingLevel = countOpening
-
-			pairDict[i] = (countPairs,True,nestingLevel)
-			nestingList.append(nestingLevel)
-			ascendingList.append(countPairs)
-		if el == closingEl:
-			nestingLevel = countOpening
-			countOpening -= 1
-
-			ascendingList.reverse()
-			countClosing = ascendingList[0]
-			del ascendingList[0]
-
-			pairDict[i] = (countClosing,False,nestingLevel)
-			nestingList.append(nestingLevel)
+    return fea_text_list
 
 
-		if nestingList.count(0) == 2:
-			nestingList = []
-			ascendingList = []
-	return pairDict
+def findNestedPairs(feaList, openingEl, closingEl):
+    # returns dict, key:index in the fea list, value: (pairNumber, is opening
+    # element, nesting level)
+    countOpening = -1
+    countClosing = -1
+    countPairs = -1
+    pairDict = {}
 
-### delete whole function?:
+    ascendingList = []
+    nestingList = []
+    for i, el in enumerate(feaList):
+
+        if el == openingEl:
+            countOpening += 1
+            countPairs += 1
+            nestingLevel = countOpening
+
+            pairDict[i] = (countPairs, True, nestingLevel)
+            nestingList.append(nestingLevel)
+            ascendingList.append(countPairs)
+        if el == closingEl:
+            nestingLevel = countOpening
+            countOpening -= 1
+
+            ascendingList.reverse()
+            countClosing = ascendingList[0]
+            del ascendingList[0]
+
+            pairDict[i] = (countClosing, False, nestingLevel)
+            nestingList.append(nestingLevel)
+
+        if nestingList.count(0) == 2:
+            nestingList = []
+            ascendingList = []
+    return pairDict
+
+# delete whole function?:
+
+
 def getFeaturesAndLookups(feaList):
-	"""
-		TODO:
-		elements zastąb następującymi kategoriami:
-		rules, lookup declarations, lookup calls
-	"""
-	openingEl,closingEl = ("{","}")
-	pairDict = findNestedPairs(feaList,openingEl,closingEl)
+    """
+            TODO:
+            elements zastąb następującymi kategoriami:
+            rules, lookup declarations, lookup calls
+    """
+    openingEl, closingEl = ("{", "}")
+    pairDict = findNestedPairs(feaList, openingEl, closingEl)
 
-	feaElements = [ None for x in range(int( len(pairDict.keys())/2 ) ) ]
-	feature = {}
-	lookup = {}
-	loadingFeatures = False
-	loadingLookup = False
+    feaElements = [None for x in range(int(len(pairDict.keys()) / 2))]
+    feature = {}
+    lookup = {}
+    loadingFeatures = False
+    loadingLookup = False
 
+    # próbjuję się pozbyć powtórek w "elements"
+    countDeletingUnnecessaryLookupNames = 0  # WIP
+    lastLookupName = ""  # WIP
+    ###
+    for i, el in enumerate(feaList):
+        if i in pairDict.keys():
+            pairNum, isOpening, nestNum = pairDict[i]
 
-	### próbjuję się pozbyć powtórek w "elements"
-	countDeletingUnnecessaryLookupNames = 0 ###WIP
-	lastLookupName = "" ###WIP
-	###
-	for i, el in enumerate(feaList):
-		if i in pairDict.keys():
-			pairNum, isOpening, nestNum = pairDict[i]
+            if feaList[i - 2] == "feature" and isOpening:
+                currentNest = nestNum
+                feature = {}
+                feature["data"] = {"type": feaList[
+                    i - 2], "name": feaList[i - 1], "nestNum": currentNest, "nestIndex": pairNum}
+                loadingFeatures = True
+                loadingLookup = False
+                feature["elements"] = []
 
-			if feaList[i-2] == "feature" and isOpening:
-				currentNest = nestNum
-				feature = {}
-				feature["data"] = {"type":feaList[i-2], "name":feaList[i-1], "nestNum":currentNest, "nestIndex":pairNum }
-				loadingFeatures = True
-				loadingLookup = False
-				feature["elements"] = []
+            if feaList[i - 2] == "lookup" and isOpening:
+                currentNest = nestNum
+                lookup = {}
+                lookup["data"] = {"type": feaList[
+                    i - 2], "name": feaList[i - 1], "nestNum": currentNest, "nestIndex": pairNum}
+                loadingFeatures = False
+                loadingLookup = True
+                lookup["elements"] = []
+                lastLookupName = ""  # WIP
 
-			if feaList[i-2] == "lookup" and isOpening:
-				currentNest = nestNum
-				lookup = {}
-				lookup["data"] = {"type":feaList[i-2], "name":feaList[i-1], "nestNum":currentNest, "nestIndex":pairNum }
-				loadingFeatures = False
-				loadingLookup = True
-				lookup["elements"] = []
-				lastLookupName = "" ###WIP
+            if loadingFeatures and not isOpening:
+                feaElements[feature["data"]["nestIndex"]] = feature
+                loadingFeatures = False
+                feature = {}
 
+            if loadingLookup and currentNest == nestNum and not isOpening:
+                feaElements[lookup["data"]["nestIndex"]] = lookup
+                loadingLookup = False
+                if nestNum > 0:
+                    loadingFeatures = True
 
+                lastLookupName = lookup["data"]["name"]  # WIP
+                lookup = {}
 
-			if loadingFeatures and not isOpening:
-				feaElements[feature["data"]["nestIndex"]] = feature
-				loadingFeatures = False
-				feature = {}
+        if el != openingEl and el != closingEl:
 
+            if el == lastLookupName:  # WIP
+                countDeletingUnnecessaryLookupNames += 1  # WIP
+            print(countDeletingUnnecessaryLookupNames)  # WIP
+            print(lastLookupName)  # WIP
+            if loadingFeatures:
+                feature["elements"].append(el)
 
-			if loadingLookup and currentNest == nestNum and not isOpening:
-				feaElements[lookup["data"]["nestIndex"]] = lookup
-				loadingLookup = False
-				if nestNum > 0:
-					loadingFeatures = True
+            if loadingLookup:
+                lookup["elements"].append(el)
 
-				lastLookupName = lookup["data"]["name"] ###WIP
-				lookup = {}
+    # # I'm assuming that there is not such a thing as nested features:
+    for i, el in enumerate(feaElements):
+        if el["data"]["nestNum"] == 1:
+            print(type(feaElements[i - 1]))
+            prev_el = feaElements[i - 1]
+            for i, sub_el in enumerate(prev_el["elements"]):
+                if sub_el == el["data"]["type"]:
+                    prev_el["elements"][i] = el
+            feaElements.remove(el)
+    return feaElements
 
-
-
-			
-
-		if el != openingEl and el != closingEl:
-
-			if el == lastLookupName: ###WIP
-				countDeletingUnnecessaryLookupNames += 1 ###WIP
-			print(countDeletingUnnecessaryLookupNames) ###WIP
-			print(lastLookupName) ###WIP
-			if loadingFeatures:
-				feature["elements"].append(el)
-
-			if loadingLookup:
-				lookup["elements"].append(el)
-
-	# # I'm assuming that there is not such a thing as nested features:
-	for i,el in enumerate(feaElements):
-		if el["data"]["nestNum"] == 1:
-			print(type(feaElements[i-1]))
-			prev_el = feaElements[i-1]
-			for i,sub_el in enumerate(prev_el["elements"]):
-				if sub_el == el["data"]["type"]:
-					prev_el["elements"][i] = el
-			feaElements.remove(el)
-	return feaElements
 
 def getBlocks(feaList):
+	##########
+	# BLOCKS #
+	##########
 
-	# creating the dictionary of feaList items, with indexes as a key. 
-	# It will be useful in deleting by orginal index
-	indexed_feaDict = {}
-	for i,x in enumerate(feaList):
-		indexed_feaDict[i]=x
+    # creating the dictionary of feaList items, with indexes as a key.
+    # It will be useful in deleting by orginal index
+    indexed_feaDict = {}
+    for i, x in enumerate(feaList):
+        indexed_feaDict[i] = x
 
-	openingEl,closingEl = ("{","}")
-	nestedPairs = findNestedPairs(feaList,openingEl,closingEl)
-	
-	blocks = []
-	block_index = 0
-	for i in indexed_feaDict:
-		element = indexed_feaDict[i]
+    openingEl, closingEl = ("{", "}")
+    nestedPairs = findNestedPairs(feaList, openingEl, closingEl)
 
-		if element == openingEl:
-			block_descrpition = {}
-			opening_Index = i
-			closing_index = None
-			search_pairIndex,search_isOpening,search_deepLevel = (None, None, None)
-			for index in nestedPairs:
-				pairIndex, isOpening, deepLevel = nestedPairs[index]
-				if index == i:
-					search_pairIndex, search_isOpening, search_deepLevel = nestedPairs[index]
+    blocks = []
+    block_index = 0
+    for i in indexed_feaDict:
+        element = indexed_feaDict[i]
 
-				if pairIndex == search_pairIndex and isOpening != search_isOpening and deepLevel == search_deepLevel:
-					closing_index = index
-			block_descrpition["type"]      = f"{feaList[i - 2]}-block"
-			block_descrpition["name"]      = feaList[i - 1]
-			block_descrpition["deepLevel"] = search_deepLevel
-			block_descrpition["index"]     = block_index
-			block_descrpition["feaList_index_range"] = (opening_Index-2,closing_index+2)
-			block_descrpition["content"]   = feaList[opening_Index+1:closing_index]
-			blocks.append(block_descrpition)
+        if element == openingEl:
+            block_descrpition = {}
+            opening_Index = i
+            closing_index = None
+            search_pairIndex, search_isOpening, search_deepLevel = (None, None, None)
+            for index in nestedPairs:
+                pairIndex, isOpening, deepLevel = nestedPairs[index]
+                if index == i:
+                    search_pairIndex, search_isOpening, search_deepLevel = nestedPairs[
+                        index]
 
+                if pairIndex == search_pairIndex and isOpening != search_isOpening and deepLevel == search_deepLevel:
+                    closing_index = index
+            block_descrpition["type"] = f"{feaList[i - 2]}-block"
+            block_descrpition["name"] = feaList[i - 1]
+            block_descrpition["deepLevel"] = search_deepLevel
+            block_descrpition["index"] = block_index
+            block_descrpition["feaList_index_range"] = (opening_Index - 2, closing_index + 2) # range of global indexes
+            block_descrpition["content"] = feaList[opening_Index + 1:closing_index]
+            blocks.append(block_descrpition)
 
-	# deleting the blocks from indexed_feaDict
-	indexed_feaDict_wthout_blocks = copy.deepcopy(indexed_feaDict)
-	for block in blocks:
-		start_Index,end_index = block["feaList_index_range"]
-		indexes_to_remove = []
-		for i in indexed_feaDict_wthout_blocks:
+    # deleting the blocks from indexed_feaDict
+    indexed_feaDict_wthout_blocks = copy.deepcopy(indexed_feaDict)
+    blocks_0_deep = []
+    index_0_deep = 0
+    for block in blocks:
+        start_Index, end_index = block["feaList_index_range"]
+        indexes_to_remove = []
+        for i in indexed_feaDict_wthout_blocks:
 
-			if i >= start_Index and i <= end_index:
-				indexes_to_remove.append(i)
-		for i in indexes_to_remove:
-			indexed_feaDict_wthout_blocks.pop(i)
-	# for now retunrs blocks
-	return blocks
-	
+            if i >= start_Index and i <= end_index:
+                indexes_to_remove.append(i)
+        for i in indexes_to_remove:
+            indexed_feaDict_wthout_blocks.pop(i)
 
+        if block["deepLevel"] == 0:
+        	block["index"] = index_0_deep
+        	blocks_0_deep.append(block)
+        	index_0_deep += 1
 
-			
-			
-				
+    ###################################################
+	# classes declaration/classes without declaration #
+	###################################################
+    print(indexed_feaDict_wthout_blocks.values())
+    for b in blocks_0_deep: ###test
+    	print(b["type"],b["index"],b["name"]) ###test
 
-
-
-
+    return blocks
 
 
 currDir = os.path.dirname(os.path.abspath(__file__))
 feaPath = currDir + "/example.fea"
 feaList = stripFea(feaPath)
-findNestedPairs(feaList,"{","}")
+findNestedPairs(feaList, "{", "}")
 # for i,e in enumerate(feaList):
 # 	print(i,e)
 
 
 bl = getBlocks(feaList)
 
-for n in bl:
-	print(n['type'])
+# for n in bl:
+#     for m in n:
+#         print(n[m])
 
 
 # openingEl = "{"
