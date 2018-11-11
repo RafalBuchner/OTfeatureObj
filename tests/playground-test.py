dbafdc4348032449fbd42ca32a298e4f71d70a07
renual27 <rafal.buchner@gmail.com>
2018-11-12 00:08:47 +0100

    upgrade

--- tests/playground-test.py
+++ tests/playground-test.py
@@ -1,8 +1,3 @@
-import os
-import tools
-currDir = os.path.dirname(os.path.abspath(__file__))
-feaPath = currDir + "/example.fea"
-
-a = [1,2,3,4,5,6,7]
-del a[2:4]
-print(a)
+d = "dupa"
+help(str)
+print(d)
